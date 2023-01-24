0aa1aef165a91f8edb5bbe46933600db4072876e
lupilp <lupilp97@gmail.com>
2023-01-24 09:59:31 -0300

    validations

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -2,9 +2,63 @@ import React from "react";
 import styles from "../Styles/Contacto.module.css";
 import emailjs from "@emailjs/browser";
 import { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function validate(input) {
+  const errors = {};
+  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (!input.user_name || input.user_name.length < 3) {
+    errors.user_name = "Coloca tu nombre";
+  }
+
+  if (!input.user_email || input.user_email.match(mailFormat)) {
+    errors.user_email = "Coloca tu email";
+  }
+
+  if (!input.message || input.message.length < 10) {
+    errors.message = "Dejame tu mensaje";
+  }
+  return errors;
+}
 
 function Contacto() {
   const form = useRef();
+  const [input, setinput] = useState({
+    user_name: "",
+    user_email: "",
+    message: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    setErrors(
+      validate({
+        ...input,
+      })
+    );
+  }, [input]);
+
+  const handleChange = (e) => {
+    setinput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
+
+  // let btnDisabled = !(
+  //   input.user_name.length &&
+  //   input.user_email.length &&
+  //   input.message.length
+  // );
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -27,17 +81,49 @@ function Contacto() {
         }
       );
   };
+
   return (
     <div className={styles.contGeneral}>
       <div className={styles.edText}>¡Contactame!</div>
 
       <form className={styles.form} ref={form} onSubmit={sendEmail}>
-        <label>Nombre</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Mensaje</label>
-        <textarea name="message" />
+        <div className={styles.contInput}>
+          <label>Nombre</label>
+          <input
+            type="text"
+            name="user_name"
+            value={input.user_name}
+            onChange={(e) => handleChange(e)}
+          />
+          {errors.user_name && (
+            <div className={styles.error}>{errors.user_name}</div>
+          )}
+        </div>
+
+        <div className={styles.contInput}>
+          <label>Email</label>
+          <input
+            type="email"
+            name="user_email"
+            value={input.user_email}
+            onChange={(e) => handleChange(e)}
+          />
+          {errors.user_email && (
+            <div className={styles.error}>{errors.user_email}</div>
+          )}
+        </div>
+
+        <div className={styles.contInput}>
+          <label>Mensaje</label>
+          <textarea
+            name="message"
+            value={input.message}
+            onChange={(e) => handleChange(e)}
+          />
+          {errors.message && (
+            <div className={styles.error}>{errors.message}</div>
+          )}
+        </div>
         <input type="submit" value="Enviar" />
       </form>
     </div>
